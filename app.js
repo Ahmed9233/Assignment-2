f6b0cc41baefbc210cc09d5d7c81911e62b58ced
Ahmed9233 <125299424+Ahmed9233@users.noreply.github.com>
2023-04-11 02:11:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+// => 4
+
+// let a = document.getElementById('two');
+// let b = document.getElementById('three');
+// a.innerText = "brz"
+// b.innerText = "tuk"
+
+// => 5
+
+// let head = document.getElementById('hedone');
+// head.style.backgroundColor = "#000";
+// head.style.color = "#fff";
+
+// => 6
+
+// let objectS = {
+//     Name: "AHMED",
+//     RollNo: 483,
+//     Teacher: "Sir Ishaq Bohajani"
+// }
+// let Stringify = JSON.stringify(objectS);
+// localStorage.setItem(objectS, Stringify);
+// console.log(objectS);
+
+
+// => 7 & 8
+
+// let objectS = {
+//     Name: "AHMED",
+//     RollNo: 483,
+//     Teacher: "Sir Ishaq Bohajani"
+// }
+// let Stringify = JSON.stringify(objectS);
+// localStorage.setItem(objectS, Stringify);
+// console.log(objectS);
+
+// localStorage.getItem(objectS)
+
+// => 1
+
+// function Ahmed() {
+//     let x = 5
+//     return function () {
+//         x + 5
+
+//     }
+// };
+
+// let a = Ahmed();
+// console.log(a);
+
+// => 2
+
+// tot = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+// function search(num) {
+//     if (num == tot[i]) {
+//         console.log("Found");
+//     } else { console.log("Not Found") }
+// };
+// search(1);
+
+// => 3
+
+// let mypara = document.getElementById('para');
+// mypara.innerText = "Saylani Zindabad"
